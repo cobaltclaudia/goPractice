1e829d5e8f5fe19025084eb149d872abe4a0e753
claudia <ceruleancee@gmail.com>
2023-08-16 23:43:34 -0500

    comment out error

--- 03_packages/main.go
+++ 03_packages/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"math"
-
-	"github.com/cobaltclaudia/goPractice/03_packages/strutil"
+	// "github.com/cobaltclaudia/goPractice/03_packages/strutil"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.5))
 	fmt.Println(math.Ceil(2.5))
 	fmt.Println(math.Sqrt(16))
-	fmt.Println(strutil.Reverse("olleh"))
+	// fmt.Println(strutil.Reverse("olleh"))
 }
